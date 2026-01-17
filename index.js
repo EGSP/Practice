c7d40c224acc56762f231b9236bbece234aca2e9
EGSP <repsbro@gmail.com>
2026-01-17 14:16:28 +0300

    learn: Object.freeze

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let counter = 0; 
+let counter = 0;
 console.log("loaded");
 
 count();
 
-function count(){
+function count() {
     counter++;
     console.log("count");
 
@@ -11,8 +11,8 @@ function count(){
     p.textContent = counter;
 }
 
-function randomNumberTitle(){
-    const randomValue = getRandomIntInclusive(1,100000)
+function randomNumberTitle() {
+    const randomValue = getRandomIntInclusive(1, 100000)
     console.log("randomValue is: " + randomValue)
 
     document.title = "Number " + randomValue;
@@ -26,8 +26,22 @@ function getRandomIntInclusive(min, max) {
 }
 
 vars();
-function vars(){
+function vars() {
     console.log(variable);
     var variable = 10;
     console.log(variable);
 }
+
+const planetParameters = { ground: "sand", gravity: 9.81, radius:10};
+// planetParameters = {}; - error
+planetParameters.radius = 100;
+
+// freezed parameters will be a new object with freezed values
+// const freezedPlanetParameters = Object.freeze({...planetParameters});
+// freezed parameters will be a link to planet parameters. Planet parameters will be freezed
+const freezedPlanetParameters = Object.freeze(planetParameters);
+freezedPlanetParameters.radius = 1000; // no error but value will not change
+console.log(freezedPlanetParameters);
+planetParameters.radius = 555; // no error but value will not change
+console.log(planetParameters);
+console.log(freezedPlanetParameters);
