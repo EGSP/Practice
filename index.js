3398b19399ba792b618d2dce8401fbdacd41e4aa
EGSP <repsbro@gmail.com>
2026-01-17 13:53:01 +0300

    learn: how to execute js functions in html

---
+++ index.js
@@ -0,0 +1,26 @@
+let counter = 0; 
+console.log("loaded");
+
+count();
+
+function count(){
+    counter++;
+    console.log("count");
+
+    const p = document.querySelector("#counter");
+    p.textContent = counter;
+}
+
+function randomNumberTitle(){
+    const randomValue = getRandomIntInclusive(1,100000)
+    console.log("randomValue is: " + randomValue)
+
+    document.title = "Number " + randomValue;
+}
+
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
